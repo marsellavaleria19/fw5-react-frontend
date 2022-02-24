aa9d5571cabc529b41d5cf2fdd4c953055f80aaf
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-02-24 16:49:16 +0700

    feat : update router in file app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Profiler } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -11,10 +12,17 @@ import Payment from './pages/Payment'
 import History from './pages/History'
 import Profile from './pages/Profile'
 
+
 export default class App extends Component {
   render() {
     return (
-      <div><Signup/></div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Homepage/>}></Route>
+            <Route path="category" element={<Category/>}></Route>
+            <Route path="category/:id" element={<ListVehicle/>}></Route>
+        </Routes>
+      </BrowserRouter>
     )
   }
 }
