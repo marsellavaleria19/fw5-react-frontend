0c26caf023319f7eb9ec908e77a4adc6447e714e
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-02-28 10:55:21 +0700

    feat : add page sresult search and filter

---
+++ src/pages/Search.js
@@ -0,0 +1,193 @@
+import React, { Component, useState,useEffect } from 'react'
+import Layout from '../component/Layout'
+import {default as axios} from 'axios'
+import {useSearchParams } from 'react-router-dom';
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
+
+export  const Search = ()=> {
+    const [listSearch,setListSearch] = useState([])
+    const [page,setPage] = useState([])
+    const [searchParams,setSearchParams] = useSearchParams()
+    const [dataSearch,setDataSearch] = useState({})
+    const [filledParams,setFilledParams] = useState(['category_id','location','payment_id','name','date'])
+    const [filledForms,setFilledForms] = useState(['category_id','location','payment_id','date'])
+    const [resultFillter,setResultFillter] = useState("")
+
+    useEffect(()=>{
+        filledParams.forEach((item)=>{
+            console.log(searchParams.get(item))
+            if(searchParams.get(item)){
+                dataSearch[item] = searchParams.get(item)
+            }
+        })
+        getDataSearch(dataSearch);
+     },[]);
+
+     const handleFillter = async(event)=>{
+        event.preventDefault();
+          filledForms.forEach((item)=>{
+            if(event.target.elements[item].value){
+                dataSearch[item] = event.target.elements[item].value
+            }
+        })
+        setSearchParams(dataSearch)
+        getDataSearch(dataSearch);
+    }
+
+    const handleSort = async(event)=>{
+        event.preventDefault();
+        if(event.target.value=="PriceHigh"){
+            dataSearch.sort = "price"
+            dataSearch.order = "desc"
+        }
+        else if(event.target.value=="PriceLow"){
+            dataSearch.sort = "price"
+            dataSearch.order = "asc"
+        }
+        setSearchParams(dataSearch);
+        getDataSearch(dataSearch);
+    }
+
+    const getDataSearch = async(dataSearch)=>{
+        const url = (dataSearch)=>{
+            var result = "";
+            filledParams.forEach((item)=>{
+                if(dataSearch[item]){
+                    if(result==""){
+                        result = `${item}=${dataSearch[item]}`;
+                    }else{
+                        result+=`&${item}=${dataSearch[item]}`;
+                    }
+                }
+            })
+            if(dataSearch.sort){
+                if(result==""){
+                    result = `sort=${dataSearch.sort}&order=${dataSearch.order}`
+                }else{
+                    result += `&sort=${dataSearch.sort}&order=${dataSearch.order}`
+                }
+            }
+
+            return `http://localhost:5000/search?${result}&limit=16`
+        }
+        console.log(url(dataSearch));
+        const {data} = await axios.get(url(dataSearch));
+        setListSearch(data.results);
+        setPage(data.pageInfo);
+    }
+    return (
+        <Layout>
+            <div className="search container">
+                <div className="row mb-5">
+                    <div className="col">
+                        <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Filter
+                            </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <form onSubmit={handleFillter}>
+                                        <div className="mb-3">
+                                            <select class="form-select" name="location" aria-label="Default select example">
+                                                <option value="" style={{display:'none'}}>Location</option>
+                                                <option value="Bandung">Bandung</option>
+                                                <option value="Jakarta">Jakarta</option>
+                                                <option value="Yogyakarta">Yogyakarta</option>
+                                            </select>
+                                        </div>
+                                        <div className="mb-3">
+                                            <select class="form-select" name = "category_id" aria-label="Default select example">
+                                                <option value="" style={{display:'none'}}>Tipe</option>
+                                                <option value="1">Bike</option>
+                                                <option value="2">Cars</option>
+                                                <option value="3">Motorbike</option>
+                                            </select>
+                                        </div>
+                                        <div className="mb-3">
+                                            <select class="form-select" name="payment_id" aria-label="Default select example">
+                                                <option value="" style={{display:'none'}}>Payment</option>
+                                                <option value="1">Cash</option>
+                                                <option value="2">Transfer</option>
+                                            </select>
+                                        </div>
+                                        <div className="mb-3">
+                                            <input type="date" name="date" className='form-control' placeholder='Date'/>
+                                        </div>
+                                        <button className='button-filled mt-5'>Filter</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                            <select name="sort" className="form-select" aria-label="Default select example" onChange={handleSort}>
+                                <option value="" style={{display:'none'}}>Sort By</option>
+                                <option value="PriceHigh">Highest Price</option>
+                                <option value="PriceLow">Lowest Price</option>
+                            </select>
+                    </div>
+                </div>
+                <h2 className='mb-3'>Results : </h2>
+                <div className="row mb-3">
+                    {
+                        listSearch.map((item)=>{
+                            return(
+                                <div className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                                <div class="card">
+                                    <img src={item.photo} class="card-img-top" alt="..."/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{item.name}</h5>
+                                        <div class="card-text">Location : {item.location}</div>
+                                        <div class="card-text">Type : {item.category}</div>
+                                        <div class="card-text">Price : {item.price}</div>
+                                        <div class="card-text">Payment Type: {item.payment == null ? "-" : item.payment}</div>
+                                        <div class="card-text">Rent date : {item.rentStartDate == null && item.rentEndDate==null ? "-" : `${item.rentStartDate}-${item.rentEndDate}`}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            )
+                        })
+                    }
+{/*               
+                    <div className="col">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+            
+      </Layout>
+    )
+}
+
+export default Search
