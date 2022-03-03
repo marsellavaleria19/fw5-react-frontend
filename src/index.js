b4017283b78a636d8f12342d4ce935996e9c327c
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-03 20:00:24 +0700

    feat : add provider and store

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import './assets/css/style.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
