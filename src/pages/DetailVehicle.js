7dd5688700c997207290d61be2f77aba970201df
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-04 15:16:10 +0700

    feat : add function counter

--- src/pages/DetailVehicle.js
+++ src/pages/DetailVehicle.js
@@ -10,11 +10,17 @@ import { useParams } from 'react-router-dom'
 import Layout from '../component/Layout'
 import { Link,useNavigate } from 'react-router-dom'
 import { getDetailVehicle } from '../redux/actions/vehicle'
+import { increment,decrement } from '../redux/actions/counter'
 import { useSelector,connect } from 'react-redux'
+import Button from '../component/Button'
+import Input from '../component/Input'
+import { useDispatch } from 'react-redux'
 
-export const  DetailVehicle =  ({getDetailVehicle})=> {
+export const  DetailVehicle =  ()=> {
 
-    const {vehicle} = useSelector(state=>state)
+    const {vehicle,counter} = useSelector(state=>state)
+
+    const dispatch = useDispatch()
 
     const [dataVehicle,setDataVehicle] = useState({})
 
@@ -22,7 +28,7 @@ export const  DetailVehicle =  ({getDetailVehicle})=> {
     const navigate = useNavigate()
 
     useEffect(()=>{
-        getDetailVehicle(id)
+        dispatch(getDetailVehicle(id))
     },[]);
     
     // const getDataVehicle = async()=>{
@@ -37,7 +43,17 @@ export const  DetailVehicle =  ({getDetailVehicle})=> {
     const goToDetail = ()=>{
         window.history.back()
     }
- 
+    
+    const countIncrement = (event) =>{
+        event.preventDefault()
+        dispatch(increment())
+    }
+
+
+    const countDecrement = (event) =>{
+        event.preventDefault()
+        dispatch(decrement())
+    }
     return (
         <Layout>
             {
@@ -92,9 +108,9 @@ export const  DetailVehicle =  ({getDetailVehicle})=> {
                         </div>
                         <form>
                             <div className="form-quantity d-flex button-plus-minus">
-                                <button className="plus">+</button>
-                                <input type="number" value="2"/>
-                                <button className="minus">-</button>
+                                <Button btnVarian="plus" onClick={countIncrement}>+</Button>
+                                <Input typeInput="number" value={counter.num}/>
+                                <Button btnVarian="minus" onClick={countDecrement}>-</Button>
                             </div>
                         </form>
                     </div>
@@ -118,81 +134,8 @@ export const  DetailVehicle =  ({getDetailVehicle})=> {
                     There is no vehicle left
                 </div> 
             }
-   
-        {/* <NavbarHomeSearch/>
-        <section className="detail-vehicle container mb-5">
-            <div className="header-nav">
-                <FaChevronLeft/>
-                <span>Detail</span>
-            </div>
-            <div className="row container">
-                <div className="col-md">
-                    <div className="img-vehicle">
-                        <img src={imgDetailVehicle} alt="detail-vehicle"/>
-                    </div>
-                    <div className="row-md">
-                        <div className="d-flex justify-content-left img-vehicle-detail align-items-center">
-                            <div className="arrow-button me-3">
-                               <FaChevronLeft/>
-                            </div>
-                            <div className="d-flex flex-wrap justify-content-center">
-                                <img src={imgDetailVehicle} className="me-4" alt="Detail Vehicle"/>
-                                <img src={imgDetailVehicle} alt="Detail Vehicle"/>
-                            </div>
-                            <div className="arrow-button ms-3">
-                                <FaChevronRight/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md justify-sm-content-center">
-                    <div className="title-vehicle">
-                        <h1>Fixie-Gray Only</h1>
-                        <div className="location">Yogyakarta</div>
-                    </div>
-                    <div className="status-vehicle">
-                        <div className="text-success fw-bold mb-2">Available</div>
-                        <div className="no-prepayment">No Prepayment</div>
-                    </div>
-                    <div className="detail-info">
-                        <div className="mb-2">Capacity : 1 person</div>
-                        <div className="mb-2">Type:Bike</div>
-                        <div>Reservation before 2 PM</div>
-                    </div>
-                    <div className="price">
-                        <h1 className="text-end">Rp.78.000/day</h1>
-                    </div>
-                    <form>
-                        <div className="form-quantity d-flex button-plus-minus">
-                            <button className="plus">+</button>
-                            <input type="number" value="2"/>
-                            <button className="minus">-</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div className="button-detail">
-                <div className="row">
-                    <div className="col-lg-5 col-md-4 mb-3">
-                        <button className="button-dark btn-chat">Chat Admin</button>
-                    </div>
-                    <div className="col-lg-5 col-md mb-3">
-                        <button className="button-filled btn-reservation" onclick="window.location='./reservation.html';">Reservation</button>
-                    </div>
-                    <div className="col-lg-2 col-md-3 mb-3">
-                        <button className="button-dark btn-like d-flex justify-content-center align-items-center">
-                            <FaHeart/>Like</button>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <Footer/> */}
       </Layout>
     )
 }
 
-const mapStateToProps = state => ({vehicle:state.vehicle})
-
-const mapDispatchToProps = {getDetailVehicle}
-
-export default connect(mapStateToProps,mapDispatchToProps)(DetailVehicle)
+export default DetailVehicle
