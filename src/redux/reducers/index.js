78346cf450379e149375392b71542209535d2791
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-04 17:01:08 +0700

    feat : add counter and reservation action

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import auth from "./auth";
 import category from "./category";
 import vehicle from "./vehicle";
 import counter from "./counter";
+import reservation from "./reservation";
 
 const rootReducer = combineReducers({
     auth,
     category,
     vehicle,
-    counter
+    counter,
+    reservation
 })
 
 export default rootReducer
