29e69caff0e568317f24f48153a549d23e33b08f
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-07 05:27:30 +0700

    feat : add condition for show  navbar

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import NavbarHomeSearch from './NavbarHomeSearch'
 import Footer from './Footer'
+import Navbar from './Navbar'
+import NavbarLogin from './NavbarLogin'
+import { useSelector } from 'react-redux'
 
-export default class Layout extends Component {
-  render() {
+export const Layout  = (props) => {
+    const auth = useSelector(state=>state.auth)
     return (
       <>
-        <NavbarHomeSearch/>
-            {this.props.children}
+        {auth.token!==null ? <NavbarHomeSearch/> : <NavbarLogin/>}
+            {props.children}
         <Footer/>
       </>
     )
-  }
 }
+
+export default Layout
