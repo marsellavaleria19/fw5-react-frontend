2ad57715619aa9ffcad66235e1f876b76cb4e47f
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-22 06:59:17 +0700

    feat : update action and reducer vehicle

--- src/redux/actions/vehicle.js
+++ src/redux/actions/vehicle.js
@@ -1,41 +1,49 @@
-import { default as axios } from 'axios'
+import { default as axios } from 'axios';
+import AxiosCostum from '../../helpers/AxiosCostum';
 
-
-const { REACT_APP_URL, REACT_APP_LIMIT_VEHICLE } = process.env
+// eslint-disable-next-line no-undef
+const { REACT_APP_URL } = process.env;
 
 export const getListVehicle = () => {
-    return {
-        type: 'GET_VEHICLE',
-        payload: axios.get(`${REACT_APP_URL}/vehicles?limit=20`)
-    }
-}
+   return {
+      type: 'GET_VEHICLE',
+      payload: axios.get(`${REACT_APP_URL}/vehicles?limit=20`)
+   };
+};
 
 export const getListVehicleByMonth = () => {
-    var month = new Date().getMonth()
-    console.log(month)
-    return {
-        type: 'GET_VEHICLE',
-        payload: axios.get(`${REACT_APP_URL}/vehicles?month=${month}&limit=3`)
-    }
-}
+   var month = new Date().getMonth();
+   console.log(month);
+   return {
+      type: 'GET_VEHICLE',
+      payload: axios.get(`${REACT_APP_URL}/vehicles?month=${month}&limit=3`)
+   };
+};
 
-export const getListVehicleByCategory = (idCategory) => {
-    return {
-        type: 'GET_VEHICLE',
-        payload: axios.get(`${REACT_APP_URL}/vehicles/category/${idCategory}?limit=${REACT_APP_LIMIT_VEHICLE}`)
-    }
-}
+export const getListVehicleByCategory = (idCategory,limit) => {
+   return {
+      type: 'GET_VEHICLE',
+      payload: AxiosCostum().get(`/vehicles/category/${idCategory}?limit=${limit}`)
+   };
+};
 
 export const getListVehicleByUrl = (url) => {
-    return {
-        type: 'GET_VEHICLE_NEXT',
-        payload: axios.get(`${url}`)
-    }
-}
+   return {
+      type: 'GET_VEHICLE_NEXT',
+      payload: axios.get(`${url}`)
+   };
+};
 
 export const getDetailVehicle = (id) => {
-    return {
-        type: 'GET_VEHICLE',
-        payload: axios.get(`${REACT_APP_URL}/vehicles/${id}`)
-    }
-}
+   return {
+      type: 'GET_VEHICLE',
+      payload: axios.get(`${REACT_APP_URL}/vehicles/${id}`)
+   };
+};
+
+export const getPopularVehicle = () =>{
+   return {
+      type:'GET_VEHICLE_POPULAR',
+      payload : axios.get(`${REACT_APP_URL}/vehicles/popular?limit=4`)
+   };
+};
