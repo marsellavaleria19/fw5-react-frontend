901f29fb80b4b8b1dce2d7a4f4a479b1db23ba56
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-22 06:58:20 +0700

    fix : fix srtyle code in app

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import React, { Component, Profiler, useEffect } from 'react'
-import { BrowserRouter, Navigate, Route, Routes, useRoutes } from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import ForgotPassowrd from './pages/ForgotPassowrd'
-import Category from './pages/Category'
-import ListVehicle from './pages/ListVehicle'
-import DetailVehicle from './pages/DetailVehicle'
-import Reservation from './pages/Reservation'
-import VerifyUser from './pages/VerifyUser'
-import Payment from './pages/Payment'
-import History from './pages/History'
-import ProfileLayout from './pages/Profile'
-import Search from './pages/Search'
-import ConfirmForgotPassowrd from './pages/ConfirmForgotPassword'
-import {getDataUser } from './redux/actions/auth'
-import { useDispatch,useSelector } from 'react-redux'
-import PrivateRoute from './routers/PrivateRouter'
-import { getListCategory } from './redux/actions/category'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import ForgotPassowrd from './pages/ForgotPassowrd';
+import Category from './pages/Category';
+import ListVehicle from './pages/ListVehicle';
+import DetailVehicle from './pages/DetailVehicle';
+import Reservation from './pages/Reservation';
+import VerifyUser from './pages/VerifyUser';
+import Payment from './pages/Payment';
+import History from './pages/History';
+import ProfileLayout from './pages/Profile';
+import Search from './pages/Search';
+import ConfirmForgotPassowrd from './pages/ConfirmForgotPassword';
+import {getDataUser } from './redux/actions/auth';
+import { useDispatch,useSelector } from 'react-redux';
+import PrivateRoute from './routers/PrivateRouter';
+import { getListCategory } from './redux/actions/category';
 
 export const App = () => {
-    const auth = useSelector(state=>state.auth)
-    const dispatch = useDispatch()
+   const auth = useSelector(state=>state.auth);
+   const dispatch = useDispatch();
     
-    useEffect(()=>{
-      dispatch(getListCategory())
-    },[])
-    
-    useEffect(()=>{
-        const token = window.localStorage.getItem('token')
-        if(token){
-          dispatch({
+   useEffect(()=>{
+      dispatch(getListCategory());
+   },[]);
+
+   useEffect(()=>{
+      const token = window.localStorage.getItem('token');
+      if(token){
+         dispatch({
             type: 'LOGIN_FULFILLED',
             payload: {
-              data: {
-                results: {
-                  token
-                }
-              }
+               data: {
+                  results: {
+                     token
+                  }
+               }
             }
-          })
-          dispatch(getDataUser(token))
-        }
-    },[dispatch,auth.token])
+         });
+         dispatch(getDataUser(token));
+      }
+   },[dispatch,auth.token]);
 
   
 
 
-    return (
+   return (
       <BrowserRouter>
-        <Routes>
+         <Routes>
             <Route path="/" element={<Homepage/>}></Route>
             <Route path="category" element={<Category/>}></Route>
             <Route path="category/:id" element={<ListVehicle/>}></Route>
@@ -65,9 +65,9 @@ export const App = () => {
             <Route path="confirmforgotpassword" element={<ConfirmForgotPassowrd/>}></Route>
             <Route path="reservation/:id" element={<PrivateRoute isAuthenticated={auth.isAuthenticated}><Reservation/></PrivateRoute>}></Route>
             <Route path="payment/:id" element={<PrivateRoute isAuthenticated={auth.isAuthenticated}><Payment/></PrivateRoute>}></Route>
-        </Routes>
+         </Routes>
       </BrowserRouter>
-    )
-}
+   );
+};
 
-export default App
+export default App;
