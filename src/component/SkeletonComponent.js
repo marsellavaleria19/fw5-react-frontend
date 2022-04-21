8d51b50669d8660b54117fca1bcb24cbe8020326
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-22 05:20:56 +0700

    feat : add skeleton component

---
+++ src/component/SkeletonComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+export const SkeletonComponent = ({children,count,...rest}) => {
+    return (
+      <Skeleton height={230} containerClassName='row' count={count} className='style-skeleton' wrapper={({children})=>(<div className='col-sm-6 col-md-4 col-lg-3 mb-4'>{children}</div>)}/>
+    )
+}
+
+export default SkeletonComponent
