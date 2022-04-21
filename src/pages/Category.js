036deed2cb554950a32a3765f8343d8f484f6156
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-22 05:23:19 +0700

    fix : fix function get category and list vehicle in category page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -3,30 +3,59 @@ import {FaChevronRight} from 'react-icons/fa'
 import {default as axios} from 'axios';
 import Layout from '../component/Layout';
 import { getListCategory } from '../redux/actions/category';
-import { getListVehicle } from '../redux/actions/vehicle';
+import { getListVehicle,getListVehicleByCategory } from '../redux/actions/vehicle';
 import { Link,useNavigate } from 'react-router-dom';
 import { connect,useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import ListVehicleComponent from '../component/ListVehicleComponent';
 
 export const Category = ()=> {
     const {category,vehicle} = useSelector(state=>state)
     const dispatch = useDispatch()
-    const [listCategory,setListCategory] = useState([])
+    var [listCategoryVehicle,setListCategoryVehicle] = useState([])
     const [listVehicle,setListVehicle] = useState([])
     const navigate = useNavigate();
     const {REACT_APP_URL,REACT_APP_LIMIT_CATEGORY} = process.env 
 
     useEffect(()=>{
-        dispatch(getListCategory())
-        dispatch(getListVehicle())
+       dispatch({
+          type : 'CLEAR_VEHICLE'
+       })
+      category.listCategory.length > 0 && category.listCategory.map((itemCategory)=>{
+         dispatch(getListVehicleByCategory(itemCategory.id,REACT_APP_LIMIT_CATEGORY));
+      })
     },[]);
 
+  
+
+//     useEffect(()=>{
+//       if(category.listCategory.length > 0){
+//         getDataListVehicle();
+//       }
+//        // dispatch(getListVehicleWithCategory());
+//       // dispatch(getListVehicle())
+//   },[category.listCategory]);
+
+   //  const getDataListVehicle = ()=>{
+   //          category.listCategory.forEach(async(item)=>{
+   //             const dataVehicle = await dispatch(getListVehicleByCategory(item.id));
+   //             const dataCategoryVehicle = {category:item,dataVehicle:dataVehicle.value.data.result};
+   //             listCategoryVehicle = [...listCategoryVehicle,...[dataCategoryVehicle]];
+   //           })
+   //           setListCategoryVehicle(listCategoryVehicle);
+   // }
+
+   const getDataListVehicle = async(itemCategory)=>{
+      const dataVehicle = getListVehicleByCategory(itemCategory.id);
+      // const dataCategoryVehicle = {category:item,dataVehicle:dataVehicle.value.data.result};
+      return dataVehicle;   
+   }
+ 
     return (
-        <>
             <Layout>
-                {
-                    category.listCategory.length > 0 ? category.listCategory.map((itemCategory)=>{
-                       return (
+               {
+                  category.listCategory.length > 0 ? category.listCategory.map((itemCategory)=>{
+                     return (
                         <section className="popular-town">
                             <div className="container">
                                 <div className="row d-flex align-items-center header">
@@ -38,21 +67,17 @@ export const Category = ()=> {
                                     </div>
                                 </div>
                                 <div className="row text-center">
-                                    {
-                                       vehicle.listVehicle && vehicle.listVehicle.length > 0 && vehicle.listVehicle.filter(item=>item.category_id===itemCategory.id).filter((item,index)=>index<REACT_APP_LIMIT_CATEGORY).map((itemVehicle)=>{
-                                        return(
-                                            <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-                                                <div class="d-inline-block position-relative">
-                                                    <img src={itemVehicle.photo} className="img-fluid" alt="Car1" />
-                                                        <div class="text-title-vehicle">
-                                                            <div className="vehicle-name">{itemVehicle.name}</div>
-                                                            <div className="location">{itemVehicle.location}</div>
-                                                        </div>
-                                                </div>
-                                            </div>
-                                        )
-                                    })    
-                                    }
+                                   {
+                                      vehicle.listAllVehicle.length > 0 ? vehicle.listAllVehicle.filter((item)=>item.category_id==itemCategory.id).map((item)=>{
+                                         return(
+                                          <ListVehicleComponent item={item}/>
+                                         )
+                                      }) :
+                                      <div class="no-vehicle text-center">
+                                      There is no vehicle left
+                                  </div> 
+                                   }
+                                    {/* <ListVehicleComponent category={itemCategory} list={listCategoryVehicle}/> */}
                                 </div>
                             </div>
                         </section>
@@ -61,10 +86,9 @@ export const Category = ()=> {
                     <div class="no-vehicle text-center">
                         There is no vehicle left
                     </div> 
-                }
+               }
             </Layout>
-      </>
     )
   }
 
-  export default Category
+export default Category
