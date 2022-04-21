f2cb4b8fc39a2fce3475acb785c6829a2b38e661
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-22 06:57:24 +0700

    feat : update list vehicle component

--- src/component/ListVehicleComponent.js
+++ src/component/ListVehicleComponent.js
@@ -1,20 +1,20 @@
-import React, { Component, useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getListVehicleByCategory } from '../redux/actions/vehicle'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, {  } from 'react';
 
 export const ListVehicleComponent = ({children,item,...rest}) => {
    
-    return (
+   return (
       <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-               <div class="d-inline-block position-relative">
-                  <img src={item.photo} className="img-fluid" alt="Car1" />
-                     <div class="text-title-vehicle">
-                           <div className="vehicle-name">{item.name}</div>
-                           <div className="location">{item.location}</div>
-                     </div>
-               </div>
+         <div className="d-inline-block position-relative">
+            <img src={item.photo} className="img-fluid" alt="Car1" />
+            <div className="text-title-vehicle">
+               <div className="vehicle-name">{item.name}</div>
+               <div className="location">{item.location}</div>
             </div>
-      )
-}
+         </div>
+      </div>
+   );
+};
 
-export default ListVehicleComponent
+export default ListVehicleComponent;
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,94 +1,65 @@
-import React, { Component,useEffect,useState } from 'react'
-import {FaChevronRight} from 'react-icons/fa'
-import {default as axios} from 'axios';
+/* eslint-disable no-undef */
+import React, { useEffect } from 'react';
+import {FaChevronRight} from 'react-icons/fa';
 import Layout from '../component/Layout';
-import { getListCategory } from '../redux/actions/category';
-import { getListVehicle,getListVehicleByCategory } from '../redux/actions/vehicle';
-import { Link,useNavigate } from 'react-router-dom';
-import { connect,useSelector } from 'react-redux';
+import { getListVehicleByCategory } from '../redux/actions/vehicle';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import ListVehicleComponent from '../component/ListVehicleComponent';
 
 export const Category = ()=> {
-    const {category,vehicle} = useSelector(state=>state)
-    const dispatch = useDispatch()
-    var [listCategoryVehicle,setListCategoryVehicle] = useState([])
-    const [listVehicle,setListVehicle] = useState([])
-    const navigate = useNavigate();
-    const {REACT_APP_URL,REACT_APP_LIMIT_CATEGORY} = process.env 
+   const {category,vehicle} = useSelector(state=>state);
+   const dispatch = useDispatch();
+   const {REACT_APP_LIMIT_CATEGORY} = process.env; 
 
-    useEffect(()=>{
-       dispatch({
-          type : 'CLEAR_VEHICLE'
-       })
+   useEffect(()=>{
+      dispatch({
+         type : 'CLEAR_VEHICLE'
+      });
       category.listCategory.length > 0 && category.listCategory.map((itemCategory)=>{
          dispatch(getListVehicleByCategory(itemCategory.id,REACT_APP_LIMIT_CATEGORY));
-      })
-    },[]);
-
-  
-
-//     useEffect(()=>{
-//       if(category.listCategory.length > 0){
-//         getDataListVehicle();
-//       }
-//        // dispatch(getListVehicleWithCategory());
-//       // dispatch(getListVehicle())
-//   },[category.listCategory]);
-
-   //  const getDataListVehicle = ()=>{
-   //          category.listCategory.forEach(async(item)=>{
-   //             const dataVehicle = await dispatch(getListVehicleByCategory(item.id));
-   //             const dataCategoryVehicle = {category:item,dataVehicle:dataVehicle.value.data.result};
-   //             listCategoryVehicle = [...listCategoryVehicle,...[dataCategoryVehicle]];
-   //           })
-   //           setListCategoryVehicle(listCategoryVehicle);
-   // }
-
-   const getDataListVehicle = async(itemCategory)=>{
-      const dataVehicle = getListVehicleByCategory(itemCategory.id);
-      // const dataCategoryVehicle = {category:item,dataVehicle:dataVehicle.value.data.result};
-      return dataVehicle;   
-   }
+      });
+   },[]);
  
-    return (
-            <Layout>
-               {
-                  category.listCategory.length > 0 ? category.listCategory.map((itemCategory)=>{
-                     return (
-                        <section className="popular-town">
-                            <div className="container">
-                                <div className="row d-flex align-items-center header">
-                                    <div className="col">
-                                        <h1 className="section-title">{itemCategory.name}</h1>
-                                    </div>
-                                    <div className="col text-end">
-                                        <Link className="section-link-view" to={`/category/${itemCategory.id}`}>View all<FaChevronRight/></Link>
-                                    </div>
-                                </div>
-                                <div className="row text-center">
-                                   {
-                                      vehicle.listAllVehicle.length > 0 ? vehicle.listAllVehicle.filter((item)=>item.category_id==itemCategory.id).map((item)=>{
-                                         return(
-                                          <ListVehicleComponent item={item}/>
-                                         )
-                                      }) :
-                                      <div class="no-vehicle text-center">
+   return (
+      <Layout>
+         {
+            category.listCategory.length > 0 ? category.listCategory.map((itemCategory)=>{
+               return (
+                  <section key={itemCategory.id} className="popular-town">
+                     <div className="container">
+                        <div className="row d-flex align-items-center header">
+                           <div className="col">
+                              <h1 className="section-title">{itemCategory.name}</h1>
+                           </div>
+                           <div className="col text-end">
+                              <Link className="section-link-view" to={`/category/${itemCategory.id}`}>View all<FaChevronRight/></Link>
+                           </div>
+                        </div>
+                        <div className="row text-center">
+                           {
+                              vehicle.listAllVehicle.length > 0 ? vehicle.listAllVehicle.filter((item)=>item.category_id==itemCategory.id).map((item)=>{
+                                 return(
+                                    <ListVehicleComponent key={item.id} item={item}/>
+                                 );
+                              }) :
+                                 <div className="no-vehicle text-center">
                                       There is no vehicle left
-                                  </div> 
-                                   }
-                                    {/* <ListVehicleComponent category={itemCategory} list={listCategoryVehicle}/> */}
-                                </div>
-                            </div>
-                        </section>
-                       )
-                    }) : 
-                    <div class="no-vehicle text-center">
+                                 </div> 
+                           }
+                           {/* <ListVehicleComponent category={itemCategory} list={listCategoryVehicle}/> */}
+                        </div>
+                     </div>
+                  </section>
+               );
+            }) : 
+               <div className="no-vehicle text-center">
                         There is no vehicle left
-                    </div> 
-               }
-            </Layout>
-    )
-  }
+               </div> 
+         }
+      </Layout>
+   );
+};
 
-export default Category
+export default Category;
