d01bf5b5a12ca998e06974bd634db5876bee128f
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-22 07:00:24 +0700

    feat : update style coding in action and reducer

--- src/redux/actions/category.js
+++ src/redux/actions/category.js
@@ -1,18 +1,16 @@
-import { default as axios } from 'axios'
-import AxiosCostum from '../../helpers/AxiosCostum'
+import AxiosCostum from '../../helpers/AxiosCostum';
 
-const { REACT_APP_URL, R_VEHICLE } = process.env
 
 export const getListCategory = () => {
-    return {
-        type: 'GET_CATEGORY',
-        payload: AxiosCostum().get(`/categories?limit=20`)
-    }
-}
+   return {
+      type: 'GET_CATEGORY',
+      payload: AxiosCostum().get('/categories?limit=20')
+   };
+};
 
 export const getDetailCategory = (id) => {
-    return {
-        type: 'GET_CATEGORY',
-        payload: AxiosCostum().get((`/categories/${id}`))
-    }
-}
+   return {
+      type: 'GET_CATEGORY',
+      payload: AxiosCostum().get((`/categories/${id}`))
+   };
+};
