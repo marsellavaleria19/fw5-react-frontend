6667b480165aed49fa177cb5c158a8985e480b40
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-24 09:04:14 +0700

    feat : update list component in search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,17 +1,24 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useState,useEffect } from 'react';
 import Layout from '../component/Layout';
 import {useSearchParams } from 'react-router-dom';
 import { getListSearchFilter } from '../redux/actions/search';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link,useNavigate } from 'react-router-dom';
+import SkeletonComponent from '../component/SkeletonComponent';
+import Image from '../component/Image';
 
 export  const Search = ()=> {
-   const {search} = useSelector(state=>state);
+   const {search,location,category,payment} = useSelector(state=>state);
    const dispatch = useDispatch();
    const [searchParams,setSearchParams] = useSearchParams();
    const [dataSearch,setDataSearch] = useState({});
-   const [filledParams,setFilledParams] = useState(['category_id','location','payment_id','name','date']);
-   const [filledForms,setFilledForms] = useState(['category_id','location','payment_id','date']);
+   const [filledParams,setFilledParams] = useState(['category_id','location_id','payment_id','name','date']);
+   const [filledForms,setFilledForms] = useState(['category_id','location_id','payment_id','date']);
+   const {REACT_APP_LIMIT_CATEGORY} = process.env; 
+
+   const navigate = useNavigate();
   
    useEffect(()=>{
       filledParams.forEach((item)=>{
@@ -66,6 +73,18 @@ export  const Search = ()=> {
       dispatch(getListSearchFilter(dataSearch));
    };
 
+   const handleReset = (event)=>{
+      event.preventDefault();
+      document.getElementById('form-filter').reset();
+      setSearchParams({});
+      setDataSearch({});
+      dispatch(getListSearchFilter(''));
+   };
+
+   const goToDetail = (id)=>{
+      navigate(`/category/vehicle/${id}`);
+   };
+
    // const getDataSearch = async(dataSearch)=>{
    //     const url = (dataSearch)=>{
    //         var result = "";
@@ -109,34 +128,49 @@ export  const Search = ()=> {
                         </h2>
                         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                            <div className="accordion-body">
-                              <form onSubmit={handleFillter}>
+                              <form id="form-filter" onSubmit={handleFillter}>
                                  <div className="mb-3">
-                                    <select className="form-select" name="location" aria-label="Default select example">
+                                    <select className="form-select" name="location_id" aria-label="Default select example">
                                        <option value="" style={{display:'none'}}>Location</option>
-                                       <option value="Bandung">Bandung</option>
-                                       <option value="Jakarta">Jakarta</option>
-                                       <option value="Yogyakarta">Yogyakarta</option>
+                                       {
+                                          location.listLocation.length > 0 && location.listLocation.map((item)=>{
+                                             return (
+                                                <option key={item.id} value={item.id}>{item.location}</option>
+                                             );
+                                          })
+                                       }
+                                       
                                     </select>
                                  </div>
                                  <div className="mb-3">
                                     <select className="form-select" name = "category_id" aria-label="Default select example">
                                        <option value="" style={{display:'none'}}>Tipe</option>
-                                       <option value="1">Bike</option>
-                                       <option value="2">Cars</option>
-                                       <option value="3">Motorbike</option>
+                                       {
+                                          category.listCategory.length > 0 && category.listCategory.map((item)=>{
+                                             return (
+                                                <option key={item.id} value={item.id}>{item.name}</option>
+                                             );
+                                          })
+                                       }
                                     </select>
                                  </div>
                                  <div className="mb-3">
                                     <select className="form-select" name="payment_id" aria-label="Default select example">
                                        <option value="" style={{display:'none'}}>Payment</option>
-                                       <option value="1">Cash</option>
-                                       <option value="2">Transfer</option>
+                                       {
+                                          payment.listPaymentType.length > 0 && payment.listPaymentType.map((item)=>{
+                                             return (
+                                                <option key={item.id} value={item.id}>{item.payment}</option>
+                                             );
+                                          })
+                                       }
                                     </select>
                                  </div>
                                  <div className="mb-3">
                                     <input type="date" name="date" className='form-control' placeholder='Date'/>
                                  </div>
                                  <button className='button-filled mt-5'>Filter</button>
+                                 <button className='button-filled mt-5 ms-3' onClick={handleReset}>Reset</button>
                               </form>
                            </div>
                         </div>
@@ -154,34 +188,57 @@ export  const Search = ()=> {
             <h2 className='mb-3'>Results : </h2>
                
             <div className="row mb-3">
-               {
-                        
-                  !search.isError ?
-                     search.listSearch.length > 0 ? search.listSearch.map((item)=>{
-                        return(
-                           <div key={item.id} className="col-sm-6 col-md-4 col-lg-3 mb-3">
-                              <div className="card">
-                                 <img src={item.photo} className="card-img-top" alt="..."/>
-                                 <div className="card-body">
-                                    <h5 className="card-title">{item.name}</h5>
-                                    <div className="card-text">Location : {item.location}</div>
-                                    <div className="card-text">Type : {item.category}</div>
-                                    <div className="card-text">Price : {item.price}</div>
-                                    <div className="card-text">Payment Type: {item.payment == null ? '-' : item.payment}</div>
-                                    <div className="card-text">Rent date : {item.rentStartDate == null && item.rentEndDate==null ? '-' : `${item.rentStartDate}-${item.rentEndDate}`}</div>
-                                 </div>
-                              </div>
+               <section className="popular-town">
+                  <div className="container">
+                     {
+                        <>
+                           <div className="row text-center">
+                              {
+                                 !search.isLoading ?
+                                    search.listSearch.length > 0 ? search.listSearch.map((item)=>{
+                                       return(
+                                          <div  key={String(item.id)} onClick={()=>goToDetail(item.id)} className="col-sm-6 col-md-4 col-lg-3 mb-4">
+                                             <div className="d-inline-block position-relative">
+                                                <Image photo={item.photo} photoVarian="img-fluid" alt={`${item.name}`} />
+                                                <div className="text-title-vehicle">
+                                                   <div className="vehicle-name">{item.brand}</div>
+                                                   <div className="location">{item.location}</div>
+                                                </div>
+                                             </div>
+                                          </div>
+                                       // <div key={item.id} className="col-sm-6 col-md-4 col-lg-3 mb-3">
+                                       //    <div className="card">
+                                       //       <img src={item.photo} className="card-img-top" alt="..."/>
+                                       //       <div className="card-body">
+                                       //          <h5 className="card-title">{item.name}</h5>
+                                       //          <div className="card-text">Location : {item.location}</div>
+                                       //          <div className="card-text">Type : {item.category}</div>
+                                       //          <div className="card-text">Price : {item.price}</div>
+                                       //          <div className="card-text">Payment Type: {item.payment == null ? '-' : item.payment}</div>
+                                       //          <div className="card-text">Rent date : {item.rentStartDate == null && item.rentEndDate==null ? '-' : `${item.rentStartDate}-${item.rentEndDate}`}</div>
+                                       //       </div>
+                                       //    </div>
+                                       // </div>
+                                       );
+                                    }) : 
+                                       <div className="no-vehicle text-center">
+                                              There is no vehicle left
+                                       </div> 
+                                    :
+                                    <SkeletonComponent count={REACT_APP_LIMIT_CATEGORY}/> 
+                              }
                            </div>
-                        );
-                     }) : 
-                        <div className="no-vehicle text-center">
-                            There is no vehicle left
-                        </div> 
-                     :
-                     <div className="no-vehicle text-center">
-                        {search.errMessage}
-                     </div> 
-               }
+                           {/* {
+                              vehicle.pageInfo.next!==null ? 
+                                 <div className='text-center mt-5 mb-5'>
+                                    <Button onClick={()=>getNextData(vehicle.pageInfo.next)} btnVarian='btn-next'>Load more <FaAngleDoubleDown/></Button>
+                                 </div> : ''
+                           } */}
+                        </>   
+                     }
+                  </div>
+               </section> 
+            
                {/*               
                     <div className="col">
                         <div class="card">
