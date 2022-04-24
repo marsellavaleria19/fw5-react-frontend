fc30afee10ea08aa3787087b6deab8f191775b44
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-24 08:59:02 +0700

    fix : fix image vehicle in list vehicle component

--- src/component/ListVehicleComponent.js
+++ src/component/ListVehicleComponent.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React, {  } from 'react';
+import Image from './Image';
 
-export const ListVehicleComponent = ({children,item,...rest}) => {
+export const ListVehicleComponent = ({children,name,location,photo,...rest}) => {
    
    return (
       <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
          <div className="d-inline-block position-relative">
-            <img src={item.photo} className="img-fluid" alt="Car1" />
+            <Image photo={photo} photoVarian="img-fluid img-vehicle" alt={`${name}`} />
             <div className="text-title-vehicle">
-               <div className="vehicle-name">{item.name}</div>
-               <div className="location">{item.location}</div>
+               <div className="vehicle-name">{name}</div>
+               <div className="location">{location}</div>
             </div>
          </div>
       </div>
