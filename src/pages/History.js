fbd834ae91db976701e0e49423592a17f482789f
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-27 11:08:46 +0700

    feat : update search and filter function in history

--- src/pages/History.js
+++ src/pages/History.js
@@ -5,50 +5,83 @@ import {FaSearch} from 'react-icons/fa';
 import {FaAngleRight} from 'react-icons/fa';
 import Layout from '../component/Layout';
 import { useDispatch, useSelector } from 'react-redux';
-import { getListHistory,deleteHistory, getListHistoryFilter } from '../redux/actions/history';
+import { getListHistory,getListHistoryByUserId,deleteHistory,getListHistoryFilter, getListHistoryFilterByUserId } from '../redux/actions/history';
 import { getListVehicleByMonth } from '../redux/actions/vehicle';
 import { useParams } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
+import {Animated} from 'react-animated-css';
 import Button from '../component/Button';
+import NotFound from '../component/NotFound';
+import moment from 'moment';
 
 
 export const History  = ()=> {
 
-   const {history,vehicle} = useSelector(state=>state);
+   const {history,vehicle,auth,category,status} = useSelector(state=>state);
    const [searchParams,setSearchParams] = useSearchParams();
    const [isDelete,setIsDelete] = useState();
    const {id} = useParams();
    const [control,setControl] = useState(false);
    const dispatch = useDispatch();
+   const [listHistory,setListHistory] = useState([]);
    const [show,setShow] = useState(-1);
-   const [filledParams,setFilledParams] = useState(['category_id','location','payment_id','name','date']);
+   const [filledParams,setFilledParams] = useState(['category_id','status_id','date']);
    const [dataSearch,setDataSearch] = useState({});
    
    useEffect(()=>{
-      const token = window.localStorage.getItem('token');
-      dispatch(getListHistory(token));
-      dispatch(getListVehicleByMonth());
+      if(auth?.user!==null){
+         if(auth.user.role=='admin'){
+            dispatch(getListHistory(auth.token));
+         }else{
+            dispatch(getListHistoryByUserId(auth.token,auth.user.id));
+         }
+      }
    },[]);
 
    useEffect(()=>{
-      if(control){
-         const token = window.localStorage.getItem('token');
-         dispatch(getListHistory(token));
-         setControl(false);
-         setShow(false);
-         dispatch(getListVehicleByMonth());
+      if(history.listHistory.length > 0 && control==true){
+         setListHistory(history.listHistory);
       }
-   },[control]);
+   },[history.listHistory]);
 
-   const handleFillter = async(event)=>{
+   const handleSearch = (event)=>{
+      event.preventDefault();
+      dataSearch['search'] = event.target.elements['search'].value;
+      setSearchParams(dataSearch);
+      handleSearchFilter(dataSearch);
+   };
+
+   const handleFillter = (event)=>{
       event.preventDefault();
       filledParams.forEach((item)=>{
          if(event.target.elements[item].value){
             dataSearch[item] = event.target.elements[item].value;
          }
       });
+      handleSearchFilter(dataSearch);
+   };
+
+   const handleSearchFilter = (dataSearch)=>{
       setSearchParams(dataSearch);
-      dispatch(getListHistoryFilter(dataSearch));
+      if(auth.user.role=='admin'){
+         dispatch(getListHistoryFilter(dataSearch));
+      }else{
+         dispatch(getListHistoryFilterByUserId(dataSearch,auth.user.id,auth.token));
+      }
+      setControl(true);
+   };
+
+   const handleReset = (event)=>{
+      event.preventDefault();
+      document.getElementById('form-search').reset();
+      document.getElementById('form-filter').reset();
+      setSearchParams({});
+      setDataSearch({});
+      if(auth.user.role=='admin'){
+         dispatch(getListHistoryFilter(dataSearch,auth.token));
+      }else{
+         dispatch(getListHistoryFilterByUserId('',auth.user.id,auth.token));
+      }
    };
 
    //  const handleFillter = (event)=>{
@@ -83,47 +116,59 @@ export const History  = ()=> {
       <Layout>
          <section className="history container mb-5">
             <div className="row">
-               <div className="col-lg-8 me-4">
-                  <form onSubmit={handleFillter} className="search-filter mb-4">
+               <div className="col-lg me-4">
+                  <form onSubmit={handleSearch} id="form-search" className="search-filter mb-4">
                      <div className="d-flex position-relative">
                         <input className="form-control search-input" type="search" name="search" placeholder="Search history" aria-label="Search"/>
                         <button className="btn btn-search position-absolute" type="submit"><FaSearch/></button>
                      </div>
-                     <div className="accordion mt-4" id="accordionExample">
-                        <div className="accordion-item">
-                           <h2 className="accordion-header" id="headingOne">
-                              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  </form>
+                  <div className="accordion mt-4" id="accordionExample">
+                     <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingOne">
+                           <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                         Filter
-                              </button>
-                           </h2>
-                           <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                              <div className="accordion-body">
-                                 <form onSubmit={handleFillter}>
-                                    <div className="mb-3">
-                                       <select className="form-select" name = "category_id" aria-label="Default select example">
-                                          <option value="" style={{display:'none'}}>Vehicle Type</option>
-                                          <option value="1">Bike</option>
-                                          <option value="2">Cars</option>
-                                          <option value="3">Motorbike</option>
-                                       </select>
-                                    </div>
-                                    <div className="mb-3">
-                                       <select className="form-select" name="status_payment" aria-label="Default select example">
-                                          <option value="" style={{display:'none'}}>Status</option>
-                                          <option value="1">No Prepayment</option>
-                                          <option value="7">Payment Success</option>
-                                       </select>
-                                    </div>
-                                    <div className="mb-3">
-                                       <input type="date" name="date" className='form-control' placeholder='Date'/>
-                                    </div>
-                                    <button className='button-filled mt-5'>Filter</button>
-                                 </form>
-                              </div>
+                           </button>
+                        </h2>
+                        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                           <div className="accordion-body">
+                              <form onSubmit={handleFillter} id="form-filter">
+                                 <div className="mb-3">
+                                    <select className="form-select" name = "category_id" aria-label="Default select example">
+                                       <option value="" style={{display:'none'}}>Vehicle Type</option>
+                                       {
+                                          category.listCategory.length > 0 && category.listCategory.map((item)=>{
+                                             return(
+                                                <option key={item.id} value={item.id}>{item.name}</option>
+                                             );
+                                          })
+                                       }
+                                    </select>
+                                 </div>
+                                 <div className="mb-3">
+                                    <select className="form-select" name="status_id" aria-label="Default select example">
+                                       <option value="" style={{display:'none'}}>Status</option>
+                                       {
+                                          status.listStatus.length > 0 && status.listStatus.map((item)=>{
+                                             return(
+                                                <option key={item.id} value={item.id}>{item.status}</option>
+                                             );
+                                          })
+                                       }
+                                    </select>
+                                 </div>
+                                 <div className="mb-3">
+                                    <input type="date" name="date" className='form-control' placeholder='Date'/>
+                                 </div>
+                                 <button className='button-filled mt-5'>Filter</button>
+                              </form>
                            </div>
                         </div>
                      </div>
-                  </form>
+                  </div>
+                  <div className='mt-3'>
+                     <Button btnVarian={'button-filled w-100 fs-4'} onClick={handleReset}>Reset</Button>
+                  </div>
                   <div className="today">
                      <div className="title-date mb-4">Today</div>
                      <div className="d-flex flex-md-wrap justify-content-between list-today">
@@ -137,30 +182,32 @@ export const History  = ()=> {
                      <div className="title-date">A week ago</div>
                      <ul className="list-group list-week">
                         {
-                           history.dataHistory!==null && history.dataHistory.filter((item)=>item.user_id==id).map((item,i)=>{
+                           history.listHistory.length>0 ? history.listHistory.map((item,i)=>{
                               return(
-                                 <div className={'row mb-3'} key={item.id} onMouseLeave={handleButtonLeave} onMouseEnter={()=>handleButton(i)}>
-                                    <div className="col-xxl-9">
-                                       <div className="row detail-order">
-                                          <div className="col-md-4">
-                                             <img src={item.photo} alt="motorbike"/>
-                                          </div>
-                                          <div className="col-md">
+                                 <div className={'d-md-flex justify-content-between mb-3'} key={item.id} onMouseLeave={handleButtonLeave} onMouseEnter={()=>handleButton(i)}>
+                                    <div className="list-item">
+                                       <div className="d-flex detail-order">
+                                          <img src={item.photo} alt="motorbike"/>
+                                          <div className='ms-3'>
                                              <h5 className="card-title">{item.brand}</h5>
-                                             <div className="date">{item.rentStartDate} - {item.rentEndDate}</div>
+                                             <div className="date">{moment(item.rentStartDate).format('DD MMM YYYY')} - {moment(item.rentEndDate).format('DD MMM YYYY')}</div>
                                              <div className="prepayment">Payment : Rp. {item.prepayment.toLocaleString('id')}</div>
                                              <div className="status">{item.status}</div>
                                           </div>
                                        </div>
                                     </div>
-                                    <div className="col-md-2">
+                                    <div>
                                        {
-                                          show==i && <Button btnVarian="button-filled" onClick={()=>handleDelete(item.id)}>Delete</Button>  
+                                          show==i && 
+                                          <Animated animationIn='fadeIn' animationOut='fadeOut'>
+                                             <Button btnVarian="button-filled" onClick={()=>handleDelete(item.id)}>Delete</Button>  
+                                          </Animated>
                                        }
-                                    </div>
+                                    </div>    
                                  </div>
                               );
-                           })
+                           }) :
+                              <NotFound/>
                         }
                                
                         {/* <li className="list-group-item d-flex">
@@ -192,11 +239,11 @@ export const History  = ()=> {
                      </ul>
                   </div>
                </div>
-               <div className="col-lg new-arrival">
+               <div className="col-lg-4 new-arrival">
                   <div className="d-flex flex-column align-items-center">
                      <div className="title">New Arrival</div>
                      {
-                        vehicle.listVehicle.length > 0 && vehicle.listVehicle.map((item)=>{
+                        vehicle.listVehiclepopular.length > 0 && vehicle.listVehiclepopular.map((item)=>{
                            return(<div key={item.id} className="position-relative mb-3">
                               <img src={item.photo} alt="Car2"/>
                               <div className="text-title-vehicle ">
