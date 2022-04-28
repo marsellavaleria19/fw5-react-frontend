84e51ec104a700fadcffa90c72670a7b3296788a
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 10:42:54 +0700

    fix : fix select component

--- src/component/Select.js
+++ src/component/Select.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const Select = ({children,selectVarian,...rest})=> {
    return (
-      <select name="location" className={`form-select ${selectVarian}`} {...rest} aria-label="Default select example">{children}</select>
+      <select className={`form-select ${selectVarian}`} {...rest} aria-label="Default select example">{children}</select>
    );
 };
 
