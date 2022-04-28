585c94cd937703ec92a807ddafd3495000699b62
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 10:47:27 +0700

    feat : update bootstrap in index js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.css';
 import './index.css';
 import App from './App';
