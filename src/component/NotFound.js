a82c3158bb0e195655511c8228c0c314f3a6b059
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 10:40:24 +0700

    feat : add not found component

---
+++ src/component/NotFound.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const NotFound = () => {
+   return (
+      <div className="no-vehicle text-center">
+      There is no vehicle left
+      </div>
+   );
+};
+
+export default NotFound;
+
+
+
