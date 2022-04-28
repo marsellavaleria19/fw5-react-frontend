1250bbda7f4cec67bbbf78d1a8df6c3e8a510e5a
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 10:21:16 +0700

    feat : update case message in reducer history

--- src/redux/reducers/history.js
+++ src/redux/reducers/history.js
@@ -1,6 +1,7 @@
 const dataHistory = {
    dataHistory: null,
    isError: false,
+   isSuccess:false,
    isLoading: false,
    errMessage: null,
    message: null,
@@ -20,7 +21,6 @@ const history = (state = dataHistory, action) => {
       state.listHistory = data.result;
       state.isLoading = false;
       state.isError = false;
-      state.message = data.message;
       return {...state };
    }
    case 'HISTORY_REJECTED':
@@ -31,6 +31,56 @@ const history = (state = dataHistory, action) => {
       state.errMessage = data.message;
       state.listHistory = [];
       return {...state };
+   }
+   case 'HISTORY_DELETE_PENDING':
+   {
+      state.isLoading = true;
+      return {...state };
+   }
+   case 'HISTORY_DELETE_FULFILLED':
+   {
+      const { data } = action.payload;
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = true;
+      state.listHistory;
+      state.message = data.message;
+      return {...state };
+   }
+   case 'HISTORY_DELETE_REJECTED':
+   {
+      const { data } = action.payload.response;
+      state.isLoading = false;
+      state.isError = true;
+      state.isSuccess = false;
+      state.errMessage = data.message;
+      state.listHistory = [];
+      return {...state };
+   }
+   case 'GET_HISTORY':
+   {
+      state.listHistory;
+      return {...state};
+   }
+   case 'HISTORY_NOT_LOADING' :{
+      state.isLoading = false;
+      return {...state};
+   }
+   case 'HISTORY_MESSAGE_SUCCESS':
+   {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message;
+      return {...state};
+   }
+   case 'HISTORY_MESSAGE_ERROR':
+   {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.errMessage;
+      return {...state};
    }
    default:
    {
