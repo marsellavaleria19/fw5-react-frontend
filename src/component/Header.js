212f6f11edfd9bc3cd9d240cd42b270898f16ac6
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 10:45:30 +0700

    feat : update header component

--- src/component/Header.js
+++ src/component/Header.js
@@ -30,9 +30,6 @@ export const Header = ()=> {
       navigate(url(),{replace:true});
    };
 
-   const goToAddVehicle = ()=>{
-      navigate('/vehicle/add');
-   };
 
    return (
       <header>
@@ -64,23 +61,7 @@ export const Header = ()=> {
                         </Select>
                         <Input typeInput="date" name="date"/>
                      </div>
-                     <div className="row">
-                        <div className="col-sm-2 col-md-4 col-lg-3 col-xl-2">
-                           <Button btnVarian="button-filled" type="submit">Explore</Button>
-                        </div>
-                        {
-                           auth.user!==null && auth.user.role=='admin' &&    <div className="col-sm-2 col-md mb-3 col-lg col-xl">
-                              <Button btnVarian="button-filled" type="button" onClick={goToAddVehicle}>Add Vehicle</Button>
-                           </div>
-                        }
-                     
-                        {/* <div className="col-md-4 mb-3">
-                           {
-                              auth.user!==null && auth.user.role=='admin' && <Button btnVarian="button-filled" onClick={()=>goToAddVehicle}>Edit Password</Button>
-                           }
-                        </div> */}
-                     </div>
-                     {/* <button className="button-filled">Explore</button> */}
+                     <button className="button-filled">Explore</button>
                   </form>
                </div>
             </div>
