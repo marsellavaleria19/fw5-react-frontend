0da593776153a8c839b470175c4fbca52deb7572
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 10:39:20 +0700

    feat : update style in list vehicle component

--- src/component/ListVehicleComponent.js
+++ src/component/ListVehicleComponent.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import Image from './Image';
+import Button from '../component/Button';
+import {FaTrashAlt} from 'react-icons/fa';
 
-export const ListVehicleComponent = ({children,name,location,photo,...rest}) => {
-   
+export const ListVehicleComponent = ({children,name,location,hover,photo,...rest}) => {
+   const [show,setShow] = useState(-1);
    return (
-      <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
+      <div className="col-sm-6 col-md-4 col-lg-3 mb-4" {...rest}>
          <div className="d-inline-block position-relative">
-            <Image photo={photo} photoVarian="img-fluid img-vehicle" alt={`${name}`} />
+            <Image photo={photo} photoVarian="img-fluid" alt={`${name}`} />
             <div className="text-title-vehicle">
                <div className="vehicle-name">{name}</div>
                <div className="location">{location}</div>
