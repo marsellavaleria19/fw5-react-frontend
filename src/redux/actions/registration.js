3fbee0736560904dbf78a729917c744a3ef51722
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-29 07:52:29 +0700

    feat : update confirmation user email in registration action

--- src/redux/actions/registration.js
+++ src/redux/actions/registration.js
@@ -9,10 +9,26 @@ export const registrationProcess = (name, username, email, password) => {
    };
 };
 
-export const verifyProcess = (email, password, code) => {
-   const data = { 'email': email, 'password': password, 'code': code };
+export const verifyEmailProcess = (dataSend) => {
+   const data = { 'email': dataSend.email};
+   return {
+      type: 'VERIFY_USER_EMAIL',
+      payload: AxiosCostum().post('/auth/emailverification', qs.stringify(data))
+   };
+};
+
+export const confirmVerifyProcess = (dataSend) => {
+   const data = { 'email': dataSend.email, 'password': dataSend.password, 'code': dataSend.code };
    return {
       type: 'VERIFY_USER',
       payload: AxiosCostum().post('/auth/emailverification', qs.stringify(data))
    };
 };
+
+// export const verifyProcess = (email, password, code) => {
+//    const data = { 'email': email, 'password': password, 'code': code };
+//    return {
+//       type: 'VERIFY_USER',
+//       payload: AxiosCostum().post('/auth/emailverification', qs.stringify(data))
+//    };
+// };
