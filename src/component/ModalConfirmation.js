48a281cc0bbe2aaa663bec3c10fed7af3d9cc475
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-01 14:54:09 +0700

    feat : update message in modal confirmation component

--- src/component/ModalConfirmation.js
+++ src/component/ModalConfirmation.js
@@ -17,7 +17,7 @@ const ModalConfitmation = ({title,message,show,close,button,functionHandle}) => 
                      <RiErrorWarningFill size={100} className='modal-icon'/>
                   </div>
                   <div className='fs-1 pps fw-bold text-pallet-1'>Are you sure?</div>
-                  <div className='fs-4 pps  text-pallet-1'>{message}</div>
+                  <div className='fs-5 pps  text-pallet-1'>{message}</div>
                </div>
             </Modal.Body>
             <Modal.Footer className="modal-custom-body">
