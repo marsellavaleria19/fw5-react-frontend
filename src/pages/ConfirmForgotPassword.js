71101f760fad5cdef6d47e59499165b120aab68e
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-04 18:06:36 +0700

    feat : update moda & validation in confirm forgot password page

--- src/pages/ConfirmForgotPassword.js
+++ src/pages/ConfirmForgotPassword.js
@@ -5,30 +5,56 @@ import { confirmForgotPasswordProcess } from '../redux/actions/forgotPassword';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../component/Button';
 import { useNavigate } from 'react-router-dom';
+import { validation } from '../helpers/validation';
+import ModalLoading from '../component/ModalLoading';
+import ModalNotifError from '../component/ModalNotifError';
+import ModalNotifSuccess from '../component/ModalNotifSuccess';
 
 export const ConfirmForgotPassowrd = ()=> {
 
    const {auth} = useSelector(state=>state);
    const dispatch = useDispatch();
-   const [success,setSuccess] = useState(false);
+   const [control,setControl] = useState(false);
+   const [error,setError] = useState({});
+   const [showModalError,setShowModalError] = useState(false);
+   const [showModalSuccess,setShowModalSuccess] = useState(false);
+   const [showModalLoading,setShowModalLoading] = useState(false);
+   var [messageError,setMessageError] = useState('');
+   var [messageSuccess,setMessageSuccess] = useState('');
+   const handleCloseLoading = () => setShowModalLoading(false);
+   const handleCloseError = () => setShowModalError(false);
+   const handleCloseSuccess = () => setShowModalSuccess(false);
    const navigate = useNavigate();
 
    useEffect(()=>{
-      if(auth.isSubmitEmail){
-         dispatch({
-            type : 'FORGOT_PASSWORD_FULLFILLED'
-         });
+      setShowModalLoading(auth.isLoading);
+      if(auth.isLoading==false && control==true){
+         if(auth.isError){
+            messageError = auth.errMessage;
+            setMessageError(messageError);
+            setShowModalError(true);
+         }else{
+            messageSuccess = auth.message;
+            setMessageSuccess(messageSuccess);
+            setShowModalSuccess(true);
+         }
+         setControl(false);
       }
-   },[auth.isSubmitEmail]);
+   },[auth.isLoading]);
 
+   //handle show success modal after close
    useEffect(()=>{
-      if(success){
-         navigate('/login');
+      if(showModalSuccess==false || showModalError==false){
+         window.scrollTo(0,0);
       }
-   },[success]);
+   },[showModalSuccess,showModalError]);
 
    const goToBack = ()=>{
       window.history.back();
+   };
+
+   const goToLogin = ()=>{
+      navigate('/login');
    };
 
    const confirmForgotPasswordHandle = (event)=>{
@@ -37,8 +63,30 @@ export const ConfirmForgotPassowrd = ()=> {
       var code = event.target.elements['code'].value;
       var password = event.target.elements['password'].value;
       var confirmPassword = event.target.elements['confirm-password'].value;
-      dispatch(confirmForgotPasswordProcess(email,code,password,confirmPassword));        
-      setSuccess(true);
+      const data = {
+         email,code,password,'confirm password' : confirmPassword
+      };
+
+      const requirement = {
+         email : 'required|email',
+         code : 'required',
+         password : 'required',
+         'confirm password' : 'required'
+      };
+
+      const validate = validation(data,requirement);
+      if(Object.keys(validate).length == 0){
+         if(data.password!==data['confirm password']){
+            auth.idError = true;
+            auth.errMessage = 'password and confirm password not match';
+         }else{
+            dispatch(confirmForgotPasswordProcess(data));        
+            setControl(true);
+         }
+      }else{
+         setError(validate);
+      }
+     
    };
 
    return (
@@ -52,24 +100,29 @@ export const ConfirmForgotPassowrd = ()=> {
                   </div>
                </div>
                <div className="container">
-                  <form onSubmit={confirmForgotPasswordHandle}>
-                     {
-                        auth.isError==false && auth.isSubmitEmail && 
-                            <div className="alert alert-success" role="alert">
-                               {auth.message}
-                            </div>
-                     }
-                     <div>
+                  <ModalLoading show={showModalLoading} close={handleCloseLoading}/>
+                  {
+                     messageError!=='' && <ModalNotifError message={messageError} show={showModalError} close={handleCloseError}/> 
+                  }
+                  {
+                     messageSuccess!=='' && <ModalNotifSuccess message={messageSuccess} show={showModalSuccess} close={handleCloseSuccess} button="Go to login" functionHandle={goToLogin}/>
+                  }
+                  <form onSubmit={confirmForgotPasswordHandle} className='mt-5'>  
+                     <div className='mb-3'>
                         <input type="text" name="email" placeholder="Email" />
+                        {error!==null && error.email ? <div className="error">{error.email}</div> : '' }
                      </div>       
-                     <div>
+                     <div className='mb-3'>
                         <input type="text" name="code" placeholder="Code" />
+                        {error!==null && error.code ? <div className="error">{error.code}</div> : '' }
                      </div>
-                     <div>
+                     <div className='mb-3'>
                         <input type="password" name="password" placeholder="Password" />
+                        {error!==null && error.password ? <div className="error">{error.password}</div> : '' }
                      </div>
-                     <div>
+                     <div className='mb-3'>
                         <input type="password" name="confirm-password" placeholder="Confirm Password" />
+                        {error!==null && error['confirm password'] ? <div className="error">{error['confirm password']}</div> : '' }
                      </div>
                      <div>
                         <Button type="submit" className="button-send">Submit</Button>
