886d241d62c371b9d50625bd24254826d2a84155
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-06 19:36:15 +0700

    feat : add useEffect in layout component

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavbarHomeSearch from './NavbarHomeSearch';
 import Footer from './Footer';
 import NavbarLogin from './NavbarLogin';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import PrivateRouter from '../routers/PrivateRouter';
 
 export const Layout  = (props) => {
    const auth = useSelector(state=>state.auth);
+   const navigate = useNavigate();
+   useEffect(()=>{
+      if(auth.isLogout==true){
+         navigate('/');
+      }
+   },[auth.isLogout]);
+ 
    return (
       <>
          {auth.token!==null ? <NavbarHomeSearch/> : <NavbarLogin/>}
