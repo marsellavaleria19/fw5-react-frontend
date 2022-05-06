0d92a3401a7c8bfa128bb0dcbf5265bb68fa9fd1
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-06 19:04:11 +0700

    feat : add update status payment in payment action

--- src/redux/actions/payment.js
+++ src/redux/actions/payment.js
@@ -4,7 +4,15 @@ export const paymentUpdate = (token, payment, paymentId, id) => {
    const param = new URLSearchParams();
    param.append('prepayment', payment);
    param.append('payment_id', paymentId);
-   param.append('status_id', 2);
+   return {
+      type: 'PAYMENT',
+      payload: AxiosCostum(token).patch(`/histories/${id}`, param)
+   };
+};
+
+export const paymentStatusUpdate = (token,id) => {
+   const param = new URLSearchParams();
+   param.append('status_id',2);
    return {
       type: 'PAYMENT',
       payload: AxiosCostum(token).patch(`/histories/${id}`, param)
@@ -17,3 +25,4 @@ export const getListPaymentType = () => {
       payload: AxiosCostum().get('/payment-types?limit=20')
    };
 };
+
